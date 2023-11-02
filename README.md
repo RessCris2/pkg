d158a87c9459726607a9c2072258f9ca2251739a
RessCris2 <weifeiouyang@outlook.com>
2023-11-03 00:27:06 +0800

    add pk7 kk7

--- README.md
+++ README.md
@@ -8,3 +8,29 @@ implement steps to build packages.
 mmdetection 用的是 平面布局
 
 ![Alt text](image-1.png)
+
+
+
+## 打包& 安装
+```
+python -m build
+ 
+pip install ./dist/pk7-0.1-py3-none-any.whl 
+```
+
+在具体实验中，pkgg 可以正常 import pkgg
+pk5 可以正常 import kk5(!不能导入 pk5)
+可以从 pk5.egg-info/top_level.txt 中看到 kk5. 
+
+
+## __init__.py 中的写法
+
+如果要实现下面这种写法，而不让代码报错 kk7 不是 pk7 的属性
+```python
+>>> pk7.kk7.code.hello()
+hello world!
+```
+需要在 __init__.py 中分别写入
+![](image-2.png)
+
+![Alt text](image-3.png)
--- setup.py
+++ setup.py
@@ -1,13 +1,41 @@
 from setuptools import setup, find_packages
 
+
+# ## pk5
+# setup(
+#     name="pk5",
+#     version="0.1",
+#     # packages=find_packages(),
+#     packages=find_packages(
+#         # where="src",
+#         # include=["kk*"],  # alternatively: `exclude=['additional*']`
+#     ),
+#     package_dir={"": "src"},
+# )
+
+
+# ## pkgg
+# setup(
+#     name="pkgg",
+#     version="0.1",
+#     # packages=find_packages(),
+#     packages=find_packages(
+#         # where="src",
+#         # include=["kk*"],  # alternatively: `exclude=['additional*']`
+#     ),
+#     # package_dir={"": "pk3"}
+#     # package_dir={"": "src"},
+# )
+
+
+# pk7
 setup(
-    name="pkgg",
-    version="0.1",
+    name="pk7",
+    version="0.4",
     # packages=find_packages(),
     packages=find_packages(
-        # where="src",
-        # include=["kk*"],  # alternatively: `exclude=['additional*']`
+        where="src",
+        # include=["*"],  # alternatively: `exclude=['additional*']`
     ),
-    # package_dir={"": "pk3"}
-    # package_dir={"": "src"},
+    package_dir={"": "src"},
 )
